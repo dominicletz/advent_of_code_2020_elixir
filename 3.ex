0afc745e84f9f5491c9968eaad8063331557c465
Dominic Letz <dominic@diode.io>
2020-12-03 17:45:00 +0100

    day 3

---
+++ 3.ex
@@ -0,0 +1,18 @@
+#!/usr/bin/env elixir
+
+map = File.read!("3.csv")
+|> String.split("\n", trim: true)
+|> Enum.map(fn line ->
+  String.to_charlist(line)
+    |> Enum.map(fn char -> char == ?# end)
+end)
+|> Enum.reduce({0, 0}, fn trees, {pos, count} ->
+  if Enum.at(trees, rem(pos, length(trees))) do
+    {pos+3, count + 1}
+  else
+    {pos+3, count}
+  end
+end)
+
+{_pos, result} = map
+:io.format("~p~n", [result])
