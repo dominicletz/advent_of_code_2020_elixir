92c82b0b02db80721f584f7da2a94104e9bd1baa
Dominic Letz <dominic@diode.io>
2020-12-03 17:08:29 +0100

    day 2

---
+++ 2.ex
@@ -0,0 +1,17 @@
+#!/usr/bin/env elixir
+import String
+
+passwords = File.read!("2.csv")
+|> split("\n", trim: true)
+|> Enum.map(fn line ->
+  [limit, <<letter, ":">>, password] = split(line, " ")
+  [low, high] = split(limit, "-") |> Enum.map(&to_integer/1)
+  count = Enum.count(String.to_charlist(password), fn c -> c == letter end)
+
+  {password, count >= low and count <= high}
+end)
+
+result = Enum.filter(passwords, fn {_password, valid} -> valid end)
+  |> Enum.count()
+
+:io.format("~p~n", [result])
