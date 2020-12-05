7fbe5bc791523c7834a4f358eafca71fb4876570
Dominic Letz <dominic@diode.io>
2020-12-05 15:16:06 +0100

    day 5

---
+++ 5b.ex
@@ -0,0 +1,37 @@
+#!/usr/bin/env elixir
+result = File.read!("5.csv")
+|> String.split("\n", trim: true)
+|> Enum.map(fn <<row :: binary-size(7), col :: binary-size(3)>> ->
+  row = for <<c <- row>>, into: "" do
+    if c == ?B do
+      <<1::unsigned-size(1)>>
+    else
+      <<0::unsigned-size(1)>>
+    end
+  end
+  <<row :: unsigned-size(7)>> = row
+
+  col = for <<c <- col>>, into: "" do
+    if c == ?R do
+      <<1::unsigned-size(1)>>
+    else
+      <<0::unsigned-size(1)>>
+    end
+  end
+  <<col :: unsigned-size(3)>> = col
+
+  {row, col}
+end)
+|> Enum.map(fn {row, col} ->
+  row * 8 + col
+end)
+|> Enum.sort()
+|> Enum.reduce_while(0, fn seat, prev ->
+  if prev + 2 == seat do
+    {:halt, seat - 1}
+  else
+    {:cont, seat}
+  end
+end)
+
+:io.format("~p~n", [result])
