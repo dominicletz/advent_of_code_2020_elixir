ae6ba4cdac4d86330ae03a458c49c8dea210add7
Dominic Letz <dominic@diode.io>
2020-12-07 18:53:07 +0100

    day 7

---
+++ 7.ex
@@ -0,0 +1,39 @@
+#!/usr/bin/env elixir
+
+my_bag = "shiny gold"
+
+defmodule Day7 do
+  def parse(rest) do
+    case rest do
+      ~w(no other bags.) -> []
+      [_count, shade, color, _bags] -> [shade <> " " <> color]
+      [_count, shade, color, _bags | rest] -> [shade <> " " <> color | parse(rest)]
+    end
+  end
+
+  def contain(_tree, nil, _my_bag), do: false
+  def contain(_tree, [], _my_bag), do: false
+  def contain(tree, [node | nodes], bag) do
+    node == bag
+      or contain(tree, tree[node], bag)
+      or contain(tree, nodes, bag)
+  end
+end
+
+
+tree = File.read!("7.csv")
+|> String.split("\n", trim: true)
+|> Enum.map(fn row ->
+  [shade, color, "bags", "contain" | rest] = String.split(row)
+
+  key = shade <> " " <> color
+  contents = Day7.parse(rest)
+  {key, contents}
+end)
+|> Map.new()
+
+Enum.filter(tree, fn {_key, nodes} ->
+  Day7.contain(tree, nodes, my_bag)
+end)
+|> Enum.count()
+|> IO.inspect()
---
+++ 7b.ex
@@ -0,0 +1,34 @@
+#!/usr/bin/env elixir
+
+my_bag = "shiny gold"
+
+defmodule Day7 do
+  def parse(rest) do
+    case rest do
+      ~w(no other bags.) -> []
+      [count, shade, color, _bags] -> [{shade <> " " <> color, count}]
+      [count, shade, color, _bags | rest] -> [{shade <> " " <> color, count} | parse(rest)]
+    end
+  end
+
+  def count(_tree, nil), do: 0
+  def count(_tree, []), do: 0
+  def count(tree, [{node, cnt} | nodes]) do
+    cnt = String.to_integer(cnt)
+    cnt + (cnt * count(tree, tree[node])) + count(tree, nodes)
+  end
+end
+
+tree = File.read!("7.csv")
+|> String.split("\n", trim: true)
+|> Enum.map(fn row ->
+  [shade, color, "bags", "contain" | rest] = String.split(row)
+
+  key = shade <> " " <> color
+  contents = Day7.parse(rest)
+  {key, contents}
+end)
+|> Map.new()
+
+Day7.count(tree, tree[my_bag])
+|> IO.inspect()
