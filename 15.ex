dad9425f8abf18ec82aaca0ddc6f3a6b3b8ed8ff
Dominic Letz <dominic@diode.io>
2020-12-15 08:51:25 +0100

    day 15

---
+++ 15.ex
@@ -0,0 +1,28 @@
+#!/usr/bin/env elixir
+defmodule Day do
+  def run() do
+    [nums] = File.read!("15.csv")
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn row ->
+      String.split(row, ",") |> Enum.map(&String.to_integer/1)
+    end)
+
+    map = Enum.with_index(nums)
+      |> Enum.take(length(nums) - 1)
+      |> Enum.reduce(%{}, fn {num, i}, map ->
+        Map.put(map, num, {i, 1})
+      end)
+
+    {_, ret} = Enum.reduce(length(nums)..2019, {map, List.last(nums)}, fn turn, {map, next} ->
+      num = case map[next] do
+        nil -> 0
+        {pos, _count} -> turn - pos - 1
+      end
+      {Map.update(map, next, {turn-1, 1}, fn {_turn, count} -> {turn-1, count+1} end), num}
+    end)
+
+    IO.inspect(ret)
+  end
+end
+
+Day.run()
